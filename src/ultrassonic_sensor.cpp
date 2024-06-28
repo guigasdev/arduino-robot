6c9f8f61d007959e446ffc131f15ee4e1302b8b6
Guilherme Pereira <pgguilherme337@gmail.com>
2024-06-27 21:31:45 -0300

    Comentário do commit da minha nova branch

--- src/ultrassonic_sensor.cpp
+++ src/ultrassonic_sensor.cpp
@@ -6,15 +6,15 @@ Servo servo_11;
  
 long readUltrasonicDistance(int triggerPin, int echoPin)
 {
-  pinMode(triggerPin, OUTPUT);  // Clear the trigger
+  pinMode(triggerPin, OUTPUT);  // Limpa o trigger
   digitalWrite(triggerPin, LOW);
   delayMicroseconds(2);
-  // Sets the trigger pin to HIGH state for 10 microseconds
+  // Set delay para 2 MS
   digitalWrite(triggerPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(triggerPin, LOW);
   pinMode(echoPin, INPUT);
-  // Reads the echo pin, and returns the sound wave travel time in microseconds
+  // Leitura do PinMode
   return pulseIn(echoPin, HIGH);
 }
  
